88969c0edca41532485af9e92acb91a4b5bd440a
Aaron Tribou <aaron@triconn.net>
2014-02-06 22:51:28 -0600

    Setup sudo in ubuntu vagrant.sh

--- ubuntu-12.04/scripts/vagrant.sh
+++ ubuntu-12.04/scripts/vagrant.sh
@@ -1,6 +1,26 @@
-# vagrant.sh
-# Install vagrant ssh key
-mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+date > /etc/vagrant_box_build_time
+
+VAGRANT_USER=vagrant
+VAGRANT_HOME=/home/$VAGRANT_USER
+VAGRANT_KEY_URL=https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+
+# Create Vagrant user (if not already present)
+if ! id -u $VAGRANT_USER >/dev/null 2>&1; then
+    /usr/sbin/groupadd $VAGRANT_USER
+    /usr/sbin/useradd $VAGRANT_USER -g $VAGRANT_USER -G sudo -d $VAGRANT_HOME --create-home
+    echo "${VAGRANT_USER}:${VAGRANT_USER}" | chpasswd
+fi
+
+# Set up sudo
+cat > /etc/sudoers << EOM
+Defaults:vagrant !requiretty
+${VAGRANT_USER}        ALL=(ALL)       NOPASSWD: ALL
+EOM
+
+# Install vagrant keys
+mkdir $VAGRANT_HOME/.ssh
+chmod 700 $VAGRANT_HOME/.ssh
+cd $VAGRANT_HOME/.ssh
+wget --no-check-certificate "${VAGRANT_KEY_URL}" -O authorized_keys
+chmod 600 $VAGRANT_HOME/.ssh/authorized_keys
+chown -R $VAGRANT_USER:$VAGRANT_USER $VAGRANT_HOME/.ssh
